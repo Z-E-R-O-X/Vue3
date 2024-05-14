66c771b918962f4ff4acbee33a5069e70d45cfc7
Johan Gamboa <115954409+6amboa@users.noreply.github.com>
2024-05-14 10:44:13 -0500

    Componentes
    Se agregan 2 componentes, el primer componente es un botón que genera una alerta al hacer clic, y el otro componente aumenta y minimiza el tamaño del texto

---
+++ src/components/AgrandarTexto.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1 :style="{ fontSize: `${fontSize}px` }">Componente 2</h1>
+        <div>
+            <button @click="enlarge" style="margin-right: 10px;">Agrandar</button>
+            <button @click="reduce">Reducir</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const fontSize = ref(16);
+
+const enlarge = () => {
+    fontSize.value += 2;
+};
+
+const reduce = () => {
+    fontSize.value -= 2;
+    if (fontSize.value < 12) {
+        fontSize.value = 12;
+    }
+};
+</script>
+<style>
+button {
+  margin-bottom: 100px;
+}
+</style>
---
+++ src/components/MiComponente.vue
@@ -0,0 +1,30 @@
+<template>
+    <button @click="handleClick">{{ buttonText }}</button>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        buttonText: 'Oprime'
+      };
+    },
+    methods: {
+      handleClick() {
+        alert('¡Por que tan obediente jajaja!');
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  button {
+    background-color: #007bff;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  </style>
+  
